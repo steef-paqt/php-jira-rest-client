100f1af86b96889129e70d413a225fb8369c37da
Steef de Winter <steef.dewinter@paqt.com>
2024-09-26 14:16:14 +0200

    fix stan errors: Parameter #1 $record (array) of method JiraRestApi\NoOperationMonologHandler::write() is not contravariant with parameter #1 $record (Monolog\LogRecord) of method Monolog\Handler\AbstractProcessingHandler::write().

--- src/NoOperationMonologHandler.php
+++ src/NoOperationMonologHandler.php
@@ -3,17 +3,14 @@
 namespace JiraRestApi;
 
 use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\LogRecord;
 
 class NoOperationMonologHandler extends AbstractProcessingHandler
 {
     /**
      * Writes the record down to the log of the implementing handler.
-     *
-     * @param array $record
-     *
-     * @return void
      */
-    protected function write(array $record): void
+    protected function write(LogRecord $record): void
     {
         // do nothing
     }
